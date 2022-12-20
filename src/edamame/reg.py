cb82b59c67faeb47b7e6601e1e3a996dfa50ad92
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2022-12-20 16:48:05 +0100

    modified metrics

--- src/edamame/reg.py
+++ src/edamame/reg.py
@@ -3,8 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import seaborn as sns
 import scipy.stats as stats
-from IPython.display import display
+from IPython.display import display, Markdown
 from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error, get_scorer_names
 from sklearn.model_selection import train_test_split, GridSearchCV, KFold, cross_val_predict, cross_val_score
 # pandas options
@@ -33,13 +34,17 @@ def dataframe_review(data) -> None:
 
 class TrainRegressor:
     
-    def __init__(self, X, y):
-        self.X = X
-        self.y = y
+    def __init__(self, X_train, y_train, X_test, y_test):
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_test = X_test
+        self.y_test = y_test
         # check dataframe 
-        dataframe_review(self.X)
+        dataframe_review(self.X_train)
+        dataframe_review(self.X_test)
         # check columns type
-        dummy_control(self.X)
+        dummy_control(self.X_train)
+        dummy_control(self.X_test)
 
 
 
@@ -48,7 +53,7 @@ class TrainRegressor:
     # ------------ #
     def linear(self):
         linear = LinearRegression()
-        linear.fit(self.X, self.y)
+        linear.fit(self.X_train, self.y_train)
         # save the model in the instance attributes
         self.linear = linear
         # return step 
@@ -58,7 +63,7 @@ class TrainRegressor:
     # aggiungere sognificatività coef
     def linear_coef(self):
         intercept = ('intercept',self.linear.intercept_[0])
-        coef = list(zip(list(self.X.columns), self.linear.coef_[0]))
+        coef = list(zip(list(self.X_train.columns), self.linear.coef_[0]))
         coef = [intercept, *coef]
         df_coef = pd.DataFrame(coef)
         df_coef.columns = ['Columns', 'Linear Coef']
@@ -67,17 +72,23 @@ class TrainRegressor:
         
 
     def linear_metrics(self):
-        # R2
-        y_pred_train = self.linear.predict(self.X)
-        r2 = r2_score(self.y, y_pred_train)
+        y_pred_train = self.linear.predict(self.X_train)
+        y_pred_test = self.linear.predict(self.X_test)
+        # r2
+        r2_train = r2_score(self.y_train, y_pred_train)
+        r2_test = r2_score(self.y_test, y_pred_test)
         # MSE
-        mse = mean_squared_error(self.y, y_pred_train)
+        mse_train = mean_squared_error(self.y_train, y_pred_train)
+        mse_test = mean_squared_error(self.y_test, y_pred_test)
         # MAE
-        mae = mean_absolute_error(self.y, y_pred_train)
+        mae_train = mean_absolute_error(self.y_train, y_pred_train)
+        mae_test = mean_absolute_error(self.y_test, y_pred_test)
         # display step 
         index_label = ['R2', 'MSE', 'MAE']
-        metrics = pd.DataFrame([r2, mse, mae], index = index_label)
-        metrics.columns = ['Linear']
+        metrics = pd.DataFrame([[r2_train, r2_test], [mse_train, mse_test], [mae_train, mae_test]], index = index_label)
+        metrics.columns = ['Train','Test']
+        string = '### Linear model metrics:'
+        display(Markdown(string))
         display(metrics)
 
 
@@ -91,10 +102,10 @@ class TrainRegressor:
         lasso = Lasso()
         tuned_parameters = [{"alpha": alphas}]
         reg_lasso = GridSearchCV(lasso, tuned_parameters, cv=n_folds, refit=True, verbose=0, scoring='r2')
-        reg_lasso.fit(self.X, self.y)
+        reg_lasso.fit(self.X_train, self.y_train)
         # running the optimized model 
         lasso = Lasso(alpha = reg_lasso.best_params_['alpha'])
-        lasso.fit(self.X, self.y)
+        lasso.fit(self.X_train, self.y_train)
         # save the model in the instance attributes
         self.lasso = lasso
         # return step 
@@ -103,7 +114,7 @@ class TrainRegressor:
 
     def lasso_coef(self):
         intercept = ('intercept',self.lasso.intercept_[0])
-        coef = list(zip(list(self.X.columns), self.lasso.coef_))
+        coef = list(zip(list(self.X_train.columns), self.lasso.coef_))
         coef = [intercept, *coef]
         df_coef = pd.DataFrame(coef)
         df_coef.columns = ['Columns', 'Lasso Coef']
@@ -113,16 +124,23 @@ class TrainRegressor:
 
     def lasso_metrics(self):
         # R2
-        y_pred_train = self.lasso.predict(self.X)
-        r2 = r2_score(self.y, y_pred_train)
+        y_pred_train = self.lasso.predict(self.X_train)
+        y_pred_test = self.linear.predict(self.X_test)
+        # r2
+        r2_train = r2_score(self.y_train, y_pred_train)
+        r2_test = r2_score(self.y_test, y_pred_test)
         # MSE
-        mse = mean_squared_error(self.y, y_pred_train)
+        mse_train = mean_squared_error(self.y_train, y_pred_train)
+        mse_test = mean_squared_error(self.y_test, y_pred_test)
         # MAE
-        mae = mean_absolute_error(self.y, y_pred_train)
+        mae_train = mean_absolute_error(self.y_train, y_pred_train)
+        mae_test = mean_absolute_error(self.y_test, y_pred_test)
         # display step 
         index_label = ['R2', 'MSE', 'MAE']
-        metrics = pd.DataFrame([r2, mse, mae], index = index_label)
-        metrics.columns = [f'Lasso with alpha: {self.lasso.alpha:.4f}']
+        metrics = pd.DataFrame([[r2_train, r2_test], [mse_train, mse_test], [mae_train, mae_test]], index = index_label)
+        string = '### Lasso metrics:'
+        display(Markdown(string))
+        metrics.columns = [f'Train with alpha: {self.lasso.alpha:.4f}', 'Test']
         display(metrics)        
         
 
@@ -136,10 +154,10 @@ class TrainRegressor:
         ridge = Ridge()
         tuned_parameters = [{"alpha": alphas}]
         reg_ridge = GridSearchCV(ridge, tuned_parameters, cv=n_folds, refit=True, verbose=0, scoring='r2')
-        reg_ridge.fit(self.X, self.y)
+        reg_ridge.fit(self.X_train, self.y_train)
         # running the optimized model 
         ridge = Ridge(alpha = reg_ridge.best_params_['alpha'])
-        ridge.fit(self.X, self.y)
+        ridge.fit(self.X_train, self.y_train)
         # save the model in the instance attributes
         self.ridge = ridge
         # return step 
@@ -148,7 +166,7 @@ class TrainRegressor:
     
     def ridge_coef(self):
         intercept = ('intercept',self.ridge.intercept_[0])
-        coef = list(zip(list(self.X.columns), self.ridge.coef_))
+        coef = list(zip(list(self.X_train.columns), self.ridge.coef_))
         coef = [intercept, *coef]
         df_coef = pd.DataFrame(coef)
         df_coef.columns = ['Columns', 'Ridge Coef']
@@ -157,17 +175,67 @@ class TrainRegressor:
         
     
     def ridge_metrics(self):
-        # R2
-        y_pred_train = self.ridge.predict(self.X)
-        r2 = r2_score(self.y, y_pred_train)
+        y_pred_train = self.ridge.predict(self.X_train)
+        y_pred_test = self.linear.predict(self.X_test)
+        # r2
+        r2_train = r2_score(self.y_train, y_pred_train)
+        r2_test = r2_score(self.y_test, y_pred_test)
         # MSE
-        mse = mean_squared_error(self.y, y_pred_train)
+        mse_train = mean_squared_error(self.y_train, y_pred_train)
+        mse_test = mean_squared_error(self.y_test, y_pred_test)
         # MAE
-        mae = mean_absolute_error(self.y, y_pred_train)
+        mae_train = mean_absolute_error(self.y_train, y_pred_train)
+        mae_test = mean_absolute_error(self.y_test, y_pred_test)
         # display step 
         index_label = ['R2', 'MSE', 'MAE']
-        metrics = pd.DataFrame([r2, mse, mae], index = index_label)
-        metrics.columns = [f'Ridge with alpha: {self.ridge.alpha:.4f}']
+        metrics = pd.DataFrame([[r2_train, r2_test], [mse_train, mse_test], [mae_train, mae_test]], index = index_label)
+        metrics.columns = [f'Train with alpha: {self.ridge.alpha:.4f}', 'Test']
+        string = '### Ridge metrics:'
+        display(Markdown(string))
+        display(metrics) 
+
+
+    # ------------ #
+    # TREE model
+    # ------------ #
+    def tree(self, alpha: list[float, float, int] = [0, 0.001, 5], impurity: list = [0, 0.00001, 5],
+             n_folds: int = 5):
+        # hyperparameters gridsearch
+        alphas = np.linspace(alpha[0], alpha[1], alpha[2])
+        impurities = np.linspace(impurity[0], impurity[1], impurity[2])
+        tuned_parameters = [{"ccp_alpha": alphas, 'min_impurity_decrease': impurities}]
+        tree = DecisionTreeRegressor() 
+        reg_tree = GridSearchCV(tree, tuned_parameters, cv=n_folds, refit=True, verbose=0, scoring='r2')
+        reg_tree.fit(self.X_train, self.y_train)
+        # running the optimized model 
+        alpha = reg_tree.best_params_['ccp_alpha']
+        min_impurity= reg_tree.best_params_['min_impurity_decrease']
+        tree = DecisionTreeRegressor(ccp_alpha=alpha, min_impurity_decrease=min_impurity)
+        tree.fit(self.X_train, self.y_train)
+        # save the model in the instance attributes
+        self.tree = tree
+        # return step 
+        return tree
+
+
+    def tree_metrics(self):
+        y_pred_train = self.tree.predict(self.X_train)
+        y_pred_test = self.tree.predict(self.X_test)
+        # r2
+        r2_train = r2_score(self.y_train, y_pred_train)
+        r2_test = r2_score(self.y_test, y_pred_test)
+        # MSE
+        mse_train = mean_squared_error(self.y_train, y_pred_train)
+        mse_test = mean_squared_error(self.y_test, y_pred_test)
+        # MAE
+        mae_train = mean_absolute_error(self.y_train, y_pred_train)
+        mae_test = mean_absolute_error(self.y_test, y_pred_test)
+        # display step 
+        index_label = ['R2', 'MSE', 'MAE']
+        metrics = pd.DataFrame([[r2_train, r2_test], [mse_train, mse_test], [mae_train, mae_test]], index = index_label)
+        metrics.columns = [f'Train', 'Test']
+        string = '### Tree metrics:'
+        display(Markdown(string))
         display(metrics) 
 
 
@@ -192,7 +260,7 @@ class TrainRegressor:
                           Ridge(alpha = self.ridge.alpha)]
         # cross validation loop 
         for model in model_list:
-            cv_result = cross_val_score(model, self.X, self.y, cv=kfold, scoring="r2")
+            cv_result = cross_val_score(model, self.X_train, self.y_train, cv=kfold, scoring="r2")
             cv_mean.append(cv_result.mean())
             std.append(cv_result.std())
             score.append(cv_result)
