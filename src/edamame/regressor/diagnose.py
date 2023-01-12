4bf0eba5b7dda0ce4a22854d04138e07035e79cd
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-01-13 00:52:12 +0100

    added diagnose

---
+++ src/edamame/regressor/diagnose.py
@@ -0,0 +1,58 @@
+import pandas as pd 
+from IPython.display import display
+
+
+# ------------------- #
+#     LINEAR MODEL 
+# ------------------- #
+def check_linear(model):
+    if model.__class__.__name__ != 'LinearRegression':
+        raise TypeError('The model passed isn\'t a linear model')
+        
+
+def linear_coef(model):
+    check_linear(model)
+    intercept = ('intercept',model.intercept_[0])
+    coef = list(zip(list(model.feature_names_in_), model.coef_[0]))
+    coef = [intercept, *coef]
+    df_coef = pd.DataFrame(coef)
+    df_coef.columns = ['Columns', 'Linear Coef']
+    # display step 
+    display(df_coef)
+
+
+# ------------------- #
+#     LASSO MODEL 
+# ------------------- #
+def check_lasso(model): 
+    if model.__class__.__name__ != 'Lasso':
+        raise TypeError('The model passed isn\'t a lasso model')
+
+def lasso_coef(model):
+    check_lasso(model)
+    intercept = ('intercept',model.intercept_[0])
+    coef = list(zip(list(model.feature_names_in_), model.coef_))
+    coef = [intercept, *coef]
+    df_coef = pd.DataFrame(coef)
+    df_coef.columns = ['Columns', 'Lasso Coef']
+    # display step 
+    display(df_coef)
+
+
+
+# ------------------- #
+#     RIDGE MODEL 
+# ------------------- #
+def check_lasso(model): 
+    if model.__class__.__name__ != 'Ridge':
+        raise TypeError('The model passed isn\'t a ridge model')
+
+
+def ridge_coef(model):
+    intercept = ('intercept',model.intercept_[0])
+    coef = list(zip(list(model.feature_names_in_), model.coef_))
+    coef = [intercept, *coef]
+    df_coef = pd.DataFrame(coef)
+    df_coef.columns = ['Columns', 'Ridge Coef']
+    # display step 
+    display(df_coef)
