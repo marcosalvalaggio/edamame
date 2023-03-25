c1ef5b308250c2622492a61f18b22770411b22af
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-03-25 16:51:18 +0100

    wip

--- edamame/eda/tools.py
+++ edamame/eda/tools.py
@@ -2,51 +2,43 @@ import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import pandas as pd
+from typing import Tuple, Union, List
 
-# ----------------- #
-# load model 
-# ----------------- #
+
+
 def load_model(path: str):
     """
     The function load the model saved previously in the pickle format.
 
-    Parameters
-    ----------
-    path: str
-        Path to the model saved in .pkl
+    Args:
+        path (str): Path to the model saved in .pkl
 
-    Return
-    ----------
-        model loaded 
     """
     with open(path, 'rb') as file:
         model = pickle.load(file)
     return model 
 
 
-# ----------------- #
-# setup data for train 
-# ----------------- #
-def setup(X, y, dummy: bool = False, seed: int = 42, size: float = 0.25):
+
+def setup(X: pd.DataFrame, y: pd.DataFrame, dummy: bool = False, seed: int = 42, size: float = 0.25) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     """
     The function returns the following elements: X_train, y_train, X_test, y_test.
 
-    Parameters
-    ----------
-    X: pandas.DataFrame
-        The model matrix X (features matrix)
-    y: pandas.DataFrame
-        The target variable 
-    dummy: bool
-        If False, the function produces the OHE. If True, the dummy encoding
-    seed: int
-        Random seed to apply at the train_test_split function
-    size: float
-        Size of the test dataset 
+    Args: 
+        X (pd.DataFrame): The model matrix X (features matrix).
+        y (pd.DataFrame): The target variable.
+        dummy (bool): If False, the function produces the OHE. If True, the dummy encoding.
+        seed (int): Random seed to apply at the train_test_split function.
+        size (float): Size of the test dataset. 
 
-    Return
-    ----------
-    pandas.DataFrame, pandas.DataFrame, pandas.DataFrame, pandas.DataFrame
+    Return:
+        Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]: X_train, y_train, X_test, y_test.
+
+    Example:
+        >>> import edamame.eda as eda
+        >>> df = pd.DataFrame({'category': ['A', 'B', 'A', 'B', 'C', 'A'], 'value': [1, 2, 3, 4, 4, 5], 'target': ['A2', 'A2', 'B2', 'B2', 'A2', 'B2']})
+        >>> X, y = eda.split_and_scaling(df, 'target')
+        >>> X_train, y_train, X_test, y_test = eda.setup(X, y)
     """
     # split dataset in train and test
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=size, random_state=seed)
@@ -55,23 +47,22 @@ def setup(X, y, dummy: bool = False, seed: int = 42, size: float = 0.25):
     return X_train, y_train, X_test, y_test
 
 
-# --------------------- #
-# scale X dataset
-# --------------------- #
-def scaling(X, minmaxscaler: bool = False):
+
+def scaling(X: pd.DataFrame, minmaxscaler: bool = False) -> pd.DataFrame:
     """
-    The function returns the normalised matrix.
+    The function returns the normalised/standardized matrix.
 
-    Parameters
-    ----------
-    X: pandas.DataFrame
-        The model matrix X/X_train/X_test
-    minmaxscaler: bool
-        Select the type of scaling to apply to the numerical columns. 
+    Args:
+        X (pd.DataFrame): The model matrix X/X_train/X_test
+    minmaxscaler (bool): Select the type of scaling to apply to the numerical columns. By default is setted to the StandardScaler. If minmaxscaler is set to True the numerical columns is trasfomed to [0,1].
 
-    Return
-    ----------
-    pandas.DataFrame
+    Return:
+        pd.DataFrame
+
+    Example:
+        >>> import edamame.eda as eda
+        >>> X = pd.DataFrame({'category': ['A', 'B', 'A', 'B', 'C', 'A'], 'value': [1, 2, 3, 4, 4, 5]})
+        >>> X = eda.scaling(X)
     """
     # dataframe check 
     dataframe_review(X)
@@ -88,21 +79,19 @@ def scaling(X, minmaxscaler: bool = False):
     return X
 
 
-# ----------------- #
-# OHE check
-# ----------------- #
-def dummy_control(data):
+
+def dummy_control(data: pd.DataFrame) -> None:
     """
     The function checks if the Pandas dataframe passed is encoded with dummy or OHE. 
     
-    Parameters
-    ----------
-    data: pandas.DataFrame 
+    Args:
+        data (pd.Dataframe): A pandas DataFrame passed in input.
 
-    Raises
-    ----------
-    TypeError
-        Dataframe with non-numerical columns
+    Raises:
+        TypeError: If the input DataFrame contains non-numerical columns.
+
+    Returns:
+        None
     """
     types = data.dtypes
     qual_col = types[types == 'object']
@@ -115,18 +104,18 @@ def dummy_control(data):
 # ----------------- #
 #  pandas dataframe check
 # ----------------- #
-def dataframe_review(data) -> None:
+def dataframe_review(data: pd.DataFrame) -> None:
     """
     The function checks if the object passed is a Pandas dataframe.
 
-    Parameters
-    ----------
-    data: A pandas dataframe 
+    Args:
+        data (pd.Dataframe): A pandas DataFrame passed in input.
 
-    Raises
-    ----------
-    TypeError
-        The data loaded is not a DataFrame
+    Raises:
+        TypeError: If the input DataFrame contains non-numerical columns.
+        
+    Returns:
+        None
     """
     if data.__class__.__name__ == 'DataFrame':
         pass
